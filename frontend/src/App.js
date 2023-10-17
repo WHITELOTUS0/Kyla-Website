b918fd56c3f3b8a56f98009dd327b7db817d5fbd
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-10-17 21:01:31 +0300

    app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Home from './pages/Home';
+import About from './pages/About';
+import MatchQuiz from './pages/MatchQuiz';
+import SignUpLogin from './pages/SignUpLogin';
+// other imports...
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        {/* Navigation, Footer, and other components */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/quiz">
+            <MatchQuiz />
+          </Route>
+          <Route path="/login">
+            <SignUpLogin />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
