b276b998cf4c0a53f0af77024120c7f8263c42c8
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-10-17 21:13:28 +0300

    Footer and NavBar Components

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+            <p>© 2023 by Your Name. All rights reserved.</p>
+            <p>Email: developer@example.com | Phone: 123-456-7890</p>
+        </footer>
+    );
+}
+
+export default Footer;
