bfeacc20a3e13425594cfd9706a291a608c0b1f1
deruhawulahnakato <deruhawulah@gmail.com>
2023-10-26 18:06:13 +0300

    Footer icons added

--- frontend/src/Pages/Design1/Footer.jsx
+++ frontend/src/Pages/Design1/Footer.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import PhoneRoundedIcon from '@mui/icons-material/PhoneRounded';
+import EmailIcon from '@mui/icons-material/Email';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import CopyrightIcon from '@mui/icons-material/Copyright';
 import '../../css/footer.css'
 
 function Footer() {
   return (
     <footer >
         <div className='footer'>
-            Footer
+            <span className='icon-large'><PhoneRoundedIcon/></span>
+            <span><EmailIcon/></span>
+            <span><InstagramIcon/></span>
+            <span><CopyrightIcon/></span>
         </div>
     </footer>
   )
