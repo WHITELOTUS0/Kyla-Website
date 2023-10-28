25558ee9c0bab7ba7396beea1ee7183691501db0
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-10-28 13:20:14 +0300

    More frontend modifications

--- frontend/src/Pages/Design1/Footer.jsx
+++ frontend/src/Pages/Design1/Footer.jsx
@@ -1,38 +1,25 @@
-import React from "react";
-import PhoneRoundedIcon from "@mui/icons-material/PhoneRounded";
-import EmailIcon from "@mui/icons-material/Email";
-import InstagramIcon from "@mui/icons-material/Instagram";
-import CopyrightIcon from "@mui/icons-material/Copyright";
-import "../../css/footer.css";
+import React from 'react';
+import PhoneRoundedIcon from '@mui/icons-material/PhoneRounded';
+import EmailIcon from '@mui/icons-material/Email';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import CopyrightIcon from '@mui/icons-material/Copyright';
+import '../../css/footer.css';
 
 function Footer() {
   return (
     <footer>
-      <div className="footer">
-        <span className="icon-large">
+      <div className='footer'>
+        <a href="tel:your-phone-number" className='icon-large'>
           <PhoneRoundedIcon />
-        </span>
+        </a>
+        <a href="mailto:your-email@example.com">
+          <EmailIcon />
+        </a>
+        <a href="https://www.instagram.com/your-instagram-profile">
+          <InstagramIcon />
+        </a>
         <span>
-          <a
-            href="kyla.busingye@akamom.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <EmailIcon />
-          </a>
-        </span>
-        <span>
-          <a
-            href="https://www.instagram.com/your_instagram_username/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <InstagramIcon />
-          </a>
-        </span>
-        <span>
-          <CopyrightIcon />
-          Kyla Busingye 2023
+          <CopyrightIcon />Kyla Busingye 2023
         </span>
       </div>
     </footer>
