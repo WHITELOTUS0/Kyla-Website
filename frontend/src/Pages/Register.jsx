642c514b30f01dfd2678bb199d387e41ad824e61
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-10-28 12:06:30 +0300

    Added more grade options

--- frontend/src/Pages/Register.jsx
+++ frontend/src/Pages/Register.jsx
@@ -119,9 +119,13 @@ const headers ={
                   onChange={handleInputChange}
                 >
                   <option value="">Select Grade</option>
-                  <option value="Grade 1">Grade 1</option>
-                  <option value="Grade 2">Grade 2</option>
-                  <option value="Grade 3">Grade 3</option>
+                  <option value="Grade 6">Grade 6</option>
+                  <option value="Grade 7">Grade 7</option>
+                  <option value="Grade 8">Grade 8</option>
+                  <option value="Grade 9">Grade 9</option>
+                  <option value="Grade 10">Grade 10</option>
+                  <option value="Grade 11">Grade 11</option>
+                  <option value="Grade 12">Grade 12</option>
                 {/* // Add more options as needed */}
               </select>
             </div>
