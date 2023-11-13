f8ff3ace8fa00297297d986ec14439ffcdee29fb
bjjumba <ericjjumba5@gmail.com>
2023-11-13 17:44:17 +0300

    New private email from namecheap

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -6,8 +6,8 @@ const Mailgen=require('mailgen');
 exports.sendEmail = async(to,name, user)=>{
     try {
         const transporter = nodemailer.createTransport({
-            host: 'smtp.gmail.com',
-            // port: 2525,
+            host: process.env.EMAIL_HOST,
+            port: 465,
             auth: {
                 user: process.env.EMAIL_USER, 
                 pass: process.env.EMAIL_PASSWORD, 
