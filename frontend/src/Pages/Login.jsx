8e851ff9430775b90708656127b4714cb1ecce84
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2024-01-19 21:22:24 +0300

    Update Login.jsx

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -138,7 +138,6 @@ export default function Login() {
               > */}
                   {/* Sign In */}
               {/* </button> */}
-            
               </div>
             </form>
             <div className="Remember">
